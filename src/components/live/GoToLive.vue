5470356bd05b1428607ea687663f7e1ecc4409f3
jianbang <jianbang@staff.weibo.com>
2021-07-01 16:24:18 +0800

    [feature]live开发

---
+++ src/components/live/GoToLive.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="goto_live">
+    222
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.goto_live {
+}
+</style>
