0e014e30c33c27e2ff0efdc3bd962114143e65c4
jianbang <jianbang@staff.weibo.com>
2021-08-06 17:14:22 +0800

    [bugfix]修改图片预览地址为https

--- generator/entryGenerator.js
+++ generator/entryGenerator.js
@@ -5,7 +5,7 @@ const genEntry = path.resolve(__dirname, './entry.js');
 const webDir = path.resolve(__dirname, '../beeto/');
 
 // 版本号
-const webEntry = path.resolve(webDir, `entry20210806_11.js`);
+const webEntry = path.resolve(webDir, `entry20210806_15.js`);
 const hasWebEntry = fs.existsSync(webDir);
 !hasWebEntry && fs.mkdirSync(webDir);
 
