d6106252462be729c8e8a364ad921ce022e2b092
jianbang <jianbang@staff.weibo.com>
2021-09-29 21:14:21 +0800

    [feature]线上打包去除console

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const entryGenerator = require('./generator/entryGenerator');
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   publicPath: process.env.PUBLIC_PATH,
@@ -51,6 +52,22 @@ module.exports = {
         'windows.jQuery': 'jquery',
       }),
     );
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置
+      config.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              drop_debugger: true,
+              drop_console: true, //生产环境自动删除console
+            },
+            warnings: false,
+          },
+          sourceMap: false,
+          parallel: true, //使用多进程并行运行来提高构建速度。默认并发运行数：os.cpus().length - 1。
+        }),
+      );
+    }
   },
   //允许对内部的 webpack 配置进行更细粒度的修改。
   chainWebpack: config => {
